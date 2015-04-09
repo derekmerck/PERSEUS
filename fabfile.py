21ff1b969e2c57da842c9cb5b1802e223eacdff8
Derek Merck <Derek_Merck@brown.edu>
2015-04-09 09:22:13 -0400

    Added preliminary ideas for a fab file and deployment architecture

---
+++ fabfile.py
@@ -0,0 +1,81 @@
+"""
+PERSEUS Fabfile
+Merck, Spring 2015
+
+## Deploy and starting a network node
+
+```
+$ fab configure deploy start                        # Stand up network
+$ fab --set use_docker=true configure deploy start  # Stand up network inside of Docker containers
+$ fab host1 start:pid=listener0,type=listener,controller=control0 # Startup a single pre-deployed host w/o config
+```
+
+"""
+
+# TODO: Doesn't work, untested
+
+from fabric.api import *
+
+# TODO: Build this automagically from addresses
+env.hosts = ['host1', 'host2', 'host3']
+pid_by_host = ['control0', 'listener0', 'display0']
+
+code_dir = '/PERSEUS'
+shadow_config = 'shadow.yaml'
+
+
+def host_type():
+    run('uname -s')
+
+
+def configure():
+
+    def configure_windows_host():
+        pass
+
+    def configure_linux_host():
+        run('wget https://repo.continuum.io/archive/Anaconda3-2.2.0-Linux-x86_64.sh')
+        run('./Anaconda3-2.2.0-Linux-x86_64.sh')
+        run('conda update conda matplotlib numpy')
+        run('pip install Pyro4')
+        run('apt-get update')
+        run('apt-get install git')
+
+    def configure_dow_host():
+        # Docker on windows
+        pass
+
+    def configure_dol_host():
+        # Docker on linux
+        run('sudo apt-get install dockerio')
+
+        # Start with the continuumio/anaconda distribution
+        run('docker start -p 99500 my_container /bin/bash')  # Be sure to open the right port
+        run('conda update conda matplotlib numpy')
+        run('pip install Pyro4')
+        run('apt-get update')
+        run('apt-get install git')
+
+        # Should probably just have a pre-configured container to pull and update
+        pass
+
+    # determine which host type we need to setup
+    pass
+
+
+def start():
+
+    def get_pid(host):
+        index = env.hosts.find(host)
+        return pid_by_host[index]
+
+
+    # TODO: If this is control, startup the name server
+    with cd(code_dir):
+        run("python PERSEUS -p {0}".format(get_pid(env.host)))
+
+
+def deploy():
+    with cd(code_dir):
+        run("pip install git+https://github.com/derekmerck/PERSEUS")
+        run("rsync shadow.yaml")
