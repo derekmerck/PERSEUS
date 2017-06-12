7d88ef5daac8dfe0611dc2370878f8758af982fe
aoyalowo <adewole_oyalowo@brown.edu>
2017-06-12 11:18:49 -0400

    Changed inaccurate or unnecessary information regarding licensing. Will likely be captured/explained in EULA

--- TelemetryStream/IntellivueProtocol/IntellivueDecoder.py
+++ TelemetryStream/IntellivueProtocol/IntellivueDecoder.py
@@ -1,9 +1,5 @@
 """
-Contributions:
-Contributors:
-
-Original copyright (c) 2015-2016, Uday Agrawal, Adewole Oyalowo, Asaad Lab under MIT License. See full license and associated
-project at < https://bitbucket.org/asaadneurolab/pymind/ > .
+See associated project at < https://bitbucket.org/asaadneurolab/pymind/ > .
 """
 
 from __future__ import unicode_literals
